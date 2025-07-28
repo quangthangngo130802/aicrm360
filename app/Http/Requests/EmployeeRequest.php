c15d90dbef840b215b68af3ed00fecb860086334
thangngo.dev <thangngo130802@gmail.com>
2025-07-28 17:09:57 +0700

    a

--- app/Http/Requests/EmployeeRequest.php
+++ app/Http/Requests/EmployeeRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Models\User;
 use App\Traits\ValidatesMediaPaths;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
 
 class EmployeeRequest extends FormRequest
 {
@@ -24,17 +26,58 @@ class EmployeeRequest extends FormRequest
     public function rules(): array
     {
         $id = $this->route('id', null);
+
         return [
-            'code' => ['nullable', 'string', 'max:50', "unique:users,code,{$id}"],
+            'code' => ['nullable', 'string', 'max:50'],
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'string', 'max:255'],
             'password' => [$id ? 'nullable' : 'required', 'string', 'max:255', 'min:8'],
-            'phone' => ['nullable', 'string', 'regex:/^0\d{9}$/', "unique:users,phone,{$id}"],
+            'phone' => ['nullable', 'string', 'regex:/^0\d{9}$/'],
             'address' => ['nullable', 'string', 'max:255'],
             'birthday' => ['nullable', 'date_format:d-m-Y', 'before:today'],
             'gender' => ['required', 'in:male,female,other'],
         ];
     }
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $id = $this->route('id');
+
+            $userId = $this->input('user_id') ?? auth()->id();
+            $subdomain = User::where('id', $userId)->value('subdomain');
+
+            if (!$subdomain) return;
+
+            $email = $this->input('email');
+            $phone = $this->input('phone');
+
+
+            if ($email) {
+                $exists = User::where('email', $email)
+                    ->where('subdomain', $subdomain)
+                    ->when($id, fn($q) => $q->where('id', '<>', $id))
+                    ->exists();
+
+                if ($exists) {
+                    Log::info('Email đã tồn tại');
+                    $validator->errors()->add('email', 'Email đã tồn tại trong subdomain này.');
+                }
+            }
+
+            if ($phone) {
+                $exists = User::where('phone', $phone)
+                    ->where('subdomain', $subdomain)
+                    ->when($id, fn($q) => $q->where('id', '<>', $id))
+                    ->exists();
+
+                if ($exists) {
+                    Log::info('Số điện thoại đã tồn tại');
+                    $validator->errors()->add('phone', 'Số điện thoại đã tồn tại trong subdomain này.');
+                }
+            }
+        });
+    }
+
 
     public function messages()
     {
